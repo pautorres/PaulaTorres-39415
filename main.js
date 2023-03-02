f6e42debbca58aaa4e58bf20cdceb9df1395c858
Paula Torres <paula.storres3@gmail.com>
2023-03-02 19:45:23 -0300

    arreglos al finalizar el juego

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-//desahabilitar el boton jugar por default si no se puso un nombre
+//ranking
+let ranking = [];
+const rankingJSON = localStorage.getItem("ranking"); //obtener key con data de ultimo ranking
+if (rankingJSON !== null) {
+	ranking = JSON.parse(rankingJSON);
+}
 
+//desahabilitar el boton jugar por default si no se puso un nombre
 let boton_jugar = document.getElementsByClassName("boton-jugar")[0];
 boton_jugar.disabled = true;
 
@@ -13,11 +19,7 @@ nombre_usuario.addEventListener("input", function () {
 	}
 });
 
-//ranking
-
-let ranking = [];
-ranking.push(JSON.parse(localStorage.getItem("ranking")));
-
+//poner los nombres del ranking en el html
 let primero = "";
 let segundo = "";
 let tercero = "";
@@ -35,6 +37,7 @@ rank2.textContent = segundo;
 rank3.textContent = tercero;
 rank4.textContent = quinto;
 
+//clase usuario
 class Usuario {
 	constructor(nombre, puntuacion) {
 		this.nombre = nombre;
@@ -51,11 +54,18 @@ function juego() {
 	let main_page = document.getElementsByTagName("main")[0];
 	main_page.style.display = "none";
 
-	//TIMER
+	//TIMER 30 segundo
 	let seconds = 30;
 	let timer = setInterval(function () {
 		seconds--;
 		document.getElementById("timer").textContent = seconds;
+		if (seconds === 0) {
+			//si llega a 0 el juego se termina
+			clearInterval(timer);
+			alert("Tiempo agotado. Obtuviste " + puntuacion + "/10");
+			juego.style.display = "none";
+			main_page.style.display = "block";
+		}
 	}, 1000);
 
 	//juego de 10 multiplicaciones de numeros random del 0 al 10
@@ -63,6 +73,7 @@ function juego() {
 	let respuestas = [];
 
 	for (i = 0; i < 10; i++) {
+		//generar la 10 preguntas
 		let numero_uno = parseInt(Math.random() * 10);
 		let numero_dos = parseInt(Math.random() * 10);
 
@@ -74,34 +85,31 @@ function juego() {
 	let puntuacion = 0;
 
 	function mostrar_pregunta() {
+		//mostrar la multiplicacion en el html
 		let multi = document.getElementById("multi");
 		multi.textContent = preguntas[pregunta_actual];
 	}
 
 	function validar_respuesta() {
+		//validar la respuesta que dio el usuario, si es correcta sumar un punto
 		let rta = document.getElementById("respuesta");
 		let respuesta = parseInt(rta.value);
 
 		if (respuestas[pregunta_actual] == respuesta) {
 			puntuacion++;
-			console.log("¡Bien! Tu respuesta fue:", respuesta, "Tenés:", puntuacion, " puntos.");
-		} else {
-			console.log("¡Incorrecto! La respuesta correcta era:", respuestas[pregunta_actual], ". Tenés ", puntuacion, "puntos.");
 		}
 
-		pregunta_actual++;
+		pregunta_actual++; //cambiar de pregunta
 
 		if (pregunta_actual == 10 || seconds == 0) {
-			clearInterval(timer);
-
+			//cuando no hayan mas preguntas o se acabe el tiempo finaliza el juego
 			alert("Obtuviste " + puntuacion + "/10");
 
 			let nombre = document.getElementById("nombre_jugador").value;
-
-			ranking.push(new Usuario(nombre, puntuacion));
-
+			ranking.push(new Usuario(nombre, puntuacion)); //push al array ranking del nombre de usuario y su puntuacion
 			sort();
 
+			//cambiar el html segun el nuevo ranking
 			if (ranking[0]) {
 				primero = ranking[0].nombre;
 				rank1.textContent = primero;
@@ -123,16 +131,23 @@ function juego() {
 				rank5.textContent = quinto;
 			}
 
+			//update de la key "ranking" con el nuevo array de ranking
 			const rankingJSON = JSON.stringify(ranking);
 			localStorage.setItem("ranking", rankingJSON);
+
+			//cuando se acaba el juego aparece la pantalla con el boton jugar
+			let juego = document.getElementsByClassName("juego")[0];
+			juego.style.display = "none";
+			let main_page = document.getElementsByTagName("main")[0];
+			main_page.style.display = "block";
 		} else {
 			mostrar_pregunta();
 		}
 
-		rta.value = "";
+		rta.value = ""; //borrar el valor de la respuesta para una nueva pregunta
 	}
 
-	//validar respuesta si el usuario presiona la tecla enter
+	//nueva pregunta si el usuario presiona la tecla enter
 	mostrar_pregunta();
 	let rta = document.getElementById("respuesta");
 	rta.addEventListener("keydown", function (event) {
@@ -144,7 +159,32 @@ function juego() {
 }
 
 function sort() {
+	//ordenar array ranking de mayor a menor puntuacion
 	ranking.sort(function (a, b) {
 		return b.puntuacion - a.puntuacion;
 	});
+}
+
+// si al entrar a la pagina el array ranking tiene elementos entonces mostrarlos en el html
+if (ranking.length > 0) {
+	if (ranking[0]) {
+		primero = ranking[0].nombre;
+		rank1.textContent = primero;
+	}
+	if (ranking[1]) {
+		segundo = ranking[1].nombre;
+		rank2.textContent = segundo;
+	}
+	if (ranking[2]) {
+		tercero = ranking[2].nombre;
+		rank3.textContent = tercero;
+	}
+	if (ranking[3]) {
+		cuarto = ranking[3].nombre;
+		rank4.textContent = cuarto;
+	}
+	if (ranking[4]) {
+		quinto = ranking[4].nombre;
+		rank5.textContent = quinto;
+	}
 }
